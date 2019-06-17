3db685dc3793a1fa007a1bb687b5a1475c052337
arijdb <ariane.jurado@gmail.com>
2019-06-17 15:03:40 +0200

    add to github pages

---
+++ docs/precache-manifest.602af75a19efe5ffececa04ad9c4eaaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2a6f6fe756ad1343507f13d86ec257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14efed61aee1980f4cc2",
+    "url": "/static/css/main.3784ce78.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "14efed61aee1980f4cc2",
+    "url": "/static/js/main.744ee539.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
