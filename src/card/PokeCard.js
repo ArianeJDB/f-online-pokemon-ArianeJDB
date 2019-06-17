32e12dd120a6fb040cea584806219722c3abbc7b
arijdb <ariane.jurado@gmail.com>
2019-06-17 15:02:11 +0200

    finish styles

--- src/card/PokeCard.js
+++ src/card/PokeCard.js
@@ -5,19 +5,23 @@ class PokeCard extends React.Component {
   render() {
     return (
       <div className="card_container">
-        <h2 className="card_name">{this.props.name}</h2>
-              <img src={this.props.image} alt={`Imagen de ${this.props.name}`} className="card_image" /> 
-            <ul className="types_list">
-              {this.props.types
-                .map(type => {
-                  return (
-                    <li className="type_item">
-                      <div className="tags">{type.type.name}</div>
-                    </li>
-                  );
-                })}
-            </ul>
-            <p className="id">{`ID: ${this.props.id}`}</p>
+        <div className="grey_container">
+          <img src={this.props.image} alt={`Imagen de ${this.props.name}`}  className="card_image" />
+          <p className="id">{`ID: ${this.props.id}`}</p>
+        </div>
+        <div className="white_container">
+          <h2 className="card_name">{this.props.name}</h2>
+          <ul className="types_list">
+            {this.props.types
+                  .map(type => {
+                    return (
+                      <li className="type_item">
+                        <div className="tags">{type.type.name}</div>
+                      </li>
+                    );
+                  })}
+              </ul>
+        </div>
       </div>
     );
   }
